54a94a26983040446b824e154ff34ab1bfccf105
gustavo <gustvvo@gmail.com>
2019-04-15 19:57:05 -0500

    do while

--- index.php
+++ index.php
@@ -12,9 +12,11 @@ $jobs =[
  ],
  [
   'title' => 'Python Dev',
+  'description' => 'This is awesone Job Senior xd!!',
  ],
  [
   'title' => 'Devops',
+  'description' => 'This is awesone Job Senior xd!!',
  ] 
 ];
 
@@ -81,40 +83,37 @@ if ($var1 > 2) {
           <h3 class="border-bottom-gray" >Work Experience</h3>
           <ul>
             <?php
-            $idx = 0;
+            /*$idx = 0;
              do {
-              echo '<h5><?php echo $jobs[0]['title']; ?></h5>';
-              echo '<p><?php echo $jobs[0]['description']; ?></p>';
-               $idx = $idx +1;
-             } while ($idx < 3);
+              echo '<li class="work-position">';
+              echo '<h5>' .$jobs[$idx]['title'] .'</h5>';
+              echo '<p>' .$jobs[$idx]['description']. '</p>';
+              echo '<strong>Achievements:</strong>';
+              echo '<ul>';
+                  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+                  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+                  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+                  echo '</ul>';
+                  echo '</li>';
+               
+              $idx = $idx +1;
+             } while($idx < 3);
+              */
 
-
-
+           for ($idx=0; $idx < count($jobs); $idx++) { 
+            echo '<li class="work-position">';
+            echo '<h5>' .$jobs[$idx]['title'] .'</h5>';
+            echo '<p>' .$jobs[$idx]['description']. '</p>';
+            echo '<strong>Achievements:</strong>';
+            echo '<ul>';
+                echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+                echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+                echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+                echo '</ul>';
+                echo '</li>';
+           }  
               ?>
-            <li class="work-position">
-
-
-            <li class="work-position">
-                <h5><?php echo $jobs[1]['title']; ?></h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
-                <strong>Achievements:</strong>
-                <ul>
-                  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                  <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                </ul>
-              </li>
-              <li class="work-position">
-                  <h5><?php echo $jobs[2]['title']; ?></h5>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente sed pariatur sint exercitationem eos expedita eveniet veniam ullam, quia neque facilis dicta voluptatibus. Eveniet doloremque ipsum itaque obcaecati nihil.</p>
-                  <strong>Achievements:</strong>
-                  <ul>
-                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
-                  </ul>
-                </li>
-          </ul>
+            </ul>
         </div>
         <div>
             <h3 class="border-bottom-gray">Projects</h3>
