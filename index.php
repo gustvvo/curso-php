6099d495f74e5c8565c8064f1ce2d201254aa047
gustavo <gustvvo@gmail.com>
2019-04-21 12:45:11 -0500

    clase php poo

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 
 $lastname = 'Torres';
 $name = 'Gustavo' . $lastname;
-
-
 $limintMonths = 2400;
 //var_dump($name);
 include('jobs.php');
-
 
 //$var1 = 1;
 /*
@@ -19,46 +16,7 @@ if ($var1 > 2) {
 */
 //$jobs[0][title];
 //var_dump($jobs);
-
-function getDuration($months){
-  $years =  floor($months / 12);
-  $extraMonths = $months % 12;
-   
-  if($years < 1){
-     return "$extraMonths months";
-   }elseif ($extraMonths < 1) {
-     return "$years years";
-   }else {
-    return "$years years $extraMonths months";
-   }
-
-
-}
-
-
-function printJob($job){
-  if ($job['visible'] == false) {
-    return;
-  }
-  //contenido de la funcion
-  echo '<li class = "work-position">';
-  echo '<h5>' . $job['title'] .'</h5>';
-  echo '<p>' . $job['description'] .'</p>';
-  echo '<p>' . getDuration($job['months']) .'</p>';
-  //echo '<p>' . $totalMonths .'</p>';
-  echo '<strong>Achievements:</strong>';
-  echo '<ul>';
-  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo '</ul>';
-  echo '</li>';
-}
-
-
-
 ?>
-
 
 <!doctype html>
 <html lang="en">
@@ -129,15 +87,11 @@ function printJob($job){
           $idx = 0;
             for ($idx=0; $idx < count($jobs); $idx++) { 
              // $totalMonths = $totalMonths + $jobs[$idx]['months'];
-             $totalMonths += $jobs[$idx]['months'];
-            
-
-              if ($jobs[$idx]['visible'] == false) {
-                continue;
+             $totalMonths += $jobs[$idx]->months;
+              if($totalMonths > $limintMonths) {
+                break;
               }
-            printJob($jobs[$idx]);    
-          
-
+              printJob($jobs[$idx]);    
              }
             ?>
            
--- jobs.php
+++ jobs.php
@@ -1,7 +1,40 @@
 <?php
 
+class Job{
+    private $title;
+    public $description;
+    public $visible;
+    public $months;
+
+
+    public function setTitle($t){
+        $this->title = $t;
+    }
+    public function getTitle(){
+        return $this->title;
+    }
+    
+}
+
+
+
+
+$job1 = new Job();
+$job1->setTitle('PHP Developer');
+$job1->setTitle = 'This is awesone Job Senior xd!!';
+$job1->visible = true;
+$job1->months = '13';
+
+$job2 = new Job();
+$job2->setTitle('Python Dev');
+$job2->description = 'This is awesone Job Senior xd!!';
+$job2->visible = true;
+$job2->months = '24';
+
 $jobs =[
-    [
+    $job1,
+    $job2
+    /*[
      'title' => 'PHP Developer',
      'description' => 'This is awesone Job Senior xd!!',
      'visible' => true,
@@ -30,9 +63,40 @@ $jobs =[
      'description' => 'This is awesone Job Senior xd!!',
      'visible' => true,
      'months' => 13,
-    ] 
+    ] */
    ];
 
 
+   function getDuration($months){
+    $years =  floor($months / 12);
+    $extraMonths = $months % 12;
+     
+    if($years < 1){
+       return "$extraMonths months";
+     }elseif ($extraMonths < 1) {
+       return "$years years";
+     }else {
+      return "$years years $extraMonths months";
+     }  
+  }
+  
+  function printJob($job){
+    if ($job->visible == false) {
+      return;
+    }
+    //contenido de la funcion
+    echo '<li class = "work-position">';
+    echo '<h5>' . $job->getTitle() .'</h5>';
+    echo '<p>' . $job->description .'</p>';
+    echo '<p>' . getDuration($job->months) .'</p>';
+    //echo '<p>' . $totalMonths .'</p>';
+    echo '<strong>Achievements:</strong>';
+    echo '<ul>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '</ul>';
+    echo '</li>';
+  }
 
 
